e4051ef2595295cf2edfaea29773d516ab68d50e
Shawn Tai <shawn890715@gmail.com>
2020-09-23 17:56:57 +0800

    editted Q559 for better recursion format

--- 559. Maximum Depth of N-ary Tree.py
+++ 559. Maximum Depth of N-ary Tree.py
@@ -1,16 +1,12 @@
 def sol(root):
-
-    def depth(node):
-        if node:
-            deepest = 0
-            for child in node.children:
-                child_depth = depth(child)
-                if child_depth > deepest:
-                    deepest = child_depth
-            return deepest + 1
-        else:
-            return 0
-
-    return depth(root)
+    if root:
+        deepest = 0
+        for child in root.children:
+            child_depth = sol(child)
+            if child_depth > deepest:
+                deepest = child_depth
+        return deepest + 1
+    else:
+        return 0
 
 
